d4029cdf14089ade3b61eca373b85998af8030f2
Alex Ruddick <a.ruddick@numat-tech.com>
2023-05-01 15:35:35 -0500

    Bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as in_file:
 
 setup(
     name="midas",
-    version="0.6.0",
+    version="0.6.1",
     description="Python driver for Honeywell Midas gas detectors.",
     long_description=long_description,
     long_description_content_type='text/markdown',
